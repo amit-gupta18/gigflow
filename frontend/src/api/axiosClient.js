c0a6118fb6692d13182d66c207f04f9fa251b6ce
Amit Kumar Gupta <amitgupta542173@gmail.com>
2026-01-14 21:53:23 +0530

    upadeted api base url

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: "http://localhost:8000/api",
+    baseURL: "https://gigflow-l5jw.onrender.com/api",
     headers: {
         "Content-Type": "application/json",
     },
