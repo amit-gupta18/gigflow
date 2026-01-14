cc4c31af2420db204ea6f4c9d282e774cca0a06e
Amit Kumar Gupta <amitgupta542173@gmail.com>
2026-01-14 17:10:14 +0530

    finalised api routes

--- backend/controllers/bid.controller.js
+++ backend/controllers/bid.controller.js
@@ -1,13 +1,45 @@
-function createBid(req, res) {
-    res.send("createBid")
+const Bid = require("../models/bid.model")
+const Gig = require("../models/gig.model")
+
+
+async function createBid(req, res) {
+    try {
+        const bid = await Bid.create(req.body)
+        res.status(201).json(bid)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
 }
 
-function getBids(req, res) {
-    res.send("getBids")
+// all the bids of the particular gig passed in the gigId 
+async function getBids(req, res) {
+    try {
+        // find all the bids of the particular gig passed in the gigId 
+        const gig = await Gig.findById(req.params.gigId) // find the gig by id
+        if (!gig) {
+            return res.status(404).json({ message: "Gig not found" })
+        }
+        /// from the gigID now fetch all the bids 
+        const bids = await Bid.find({ gigId: gig._id })
+        res.status(200).json(bids)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
 }
 
-function hireBid(req, res) {
-    res.send("hireBid")
+// /api/bids/:bidid/hire
+async function hireBid(req, res) {
+    try {
+        const bid = await Bid.findById(req.params.bidid)
+        if (!bid) {
+            return res.status(404).json({ message: "Bid not found" })
+        }
+        bid.status = "hired"
+        await bid.save()
+        res.status(200).json(bid)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
 }
 
 module.exports = { createBid, getBids, hireBid }
--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 8000;
 
-
+    
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
